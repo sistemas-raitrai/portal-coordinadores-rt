7b82c2e3f7d9b78dd9d9a915b2186878e7419700
Ignacio <sistemas@raitrai.cl>
2025-08-21 10:13:08 -0400

    Update firebase-init-portal.js

--- firebase-init-portal.js
+++ firebase-init-portal.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js';
+import { getStorage }     from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-storage.js';
 
 export const app = initializeApp({
   apiKey: "AIzaSyAdx9nVcV-UiGER3mcz-w9BcSSIZd-t5nE",
@@ -10,5 +11,7 @@ export const app = initializeApp({
   messagingSenderId: "438607695630",
   appId: "1:438607695630:web:862c4a6bea4bfdd3bfd15f"
 });
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app); // o: getStorage(app, 'gs://sist-op-rt.firebasestorage.app')
